4ecd6c5bd54bf168668d168e0a29066386bc7955
Kanana-Mwenda <Lizamwenda95@gmail.com>
2025-02-25 00:16:28 +0300

    commit message

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
 INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES 
-('Mr.Chocolate', 10, 'M', 'Brown', 'Calm', TRUE),
-('Rowdy', 8, 'M', 'Black', 'Aggressive', TRUE),
-('Tabitha', 6, 'F', 'Golden', 'Playful', TRUE),
-('Sergeant Brown', 12, 'M', 'Black', 'Serious', TRUE),
-('Melissa', 7, 'F', 'Brown', 'Friendly', TRUE),
-('Grinch', 5, 'M', 'Greenish', 'Grumpy', TRUE),
-('Wendy', 9, 'F', 'White', 'Gentle', TRUE),
-(NULL, 4, 'F', 'Gray', 'Reserved', TRUE);
+('Mr.Chocolate', 10, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 8, 'M', 'Black', 'Aggressive', 1),
+('Tabitha', 6, 'F', 'Golden', 'Playful', 1),
+('Sergeant Brown', 12, 'M', 'Black', 'Serious', 1),
+('Melissa', 7, 'F', 'Brown', 'Friendly', 1),
+('Grinch', 5, 'M', 'Greenish', 'Grumpy', 1),
+('Wendy', 9, 'F', 'White', 'Gentle', 1),
+(NULL, 4, 'F', 'Gray', 'Reserved', 1);

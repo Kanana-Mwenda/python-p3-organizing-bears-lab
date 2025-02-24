064320479dd620cc0c1150d971f3c029be249f61
Kanana-Mwenda <Lizamwenda95@gmail.com>
2025-02-25 00:08:08 +0300

    commit message

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES 
+('Mr.Chocolate', 10, 'M', 'Brown', 'Calm', TRUE),
+('Rowdy', 8, 'M', 'Black', 'Aggressive', TRUE),
+('Tabitha', 6, 'F', 'Golden', 'Playful', TRUE),
+('Sergeant Brown', 12, 'M', 'Black', 'Serious', TRUE),
+('Melissa', 7, 'F', 'Brown', 'Friendly', TRUE),
+('Grinch', 5, 'M', 'Greenish', 'Grumpy', TRUE),
+('Wendy', 9, 'F', 'White', 'Gentle', TRUE),
+(NULL, 4, 'F', 'Gray', 'Reserved', TRUE);
